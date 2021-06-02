aef9fc47548a3b0d7deb82cbaf800a65841aecae
ankle <pan7766118@gmail.com>
2021-06-02 17:23:41 +0800

    add login page

--- pyango/test3/booktest/urls.py
+++ pyango/test3/booktest/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^index$', views.index), # 首頁
     # url(r'^showarg(\d+)$', views.show_arg),  # 捕獲url參數，(\d+) : 位置參數
     url(r'^showarg(?P<num>\d+)$', views.show_arg),  # 捕獲url參數，(\d+) : 關鍵字參數
+    url(r'^login$', views.login), # 顯示登錄頁面
 ]
--- pyango/test3/booktest/views.py
+++ pyango/test3/booktest/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+# request就是HttpResponse類型對象
+# request包含瀏覽器的請求信息
 def index(request):
     '''首頁'''
     return render(request, 'booktest/index.html')
 
 def show_arg(request, num):
     return HttpResponse(num)
+
+def login(request):
+    '''顯示登陸頁面'''
+    return render(request, 'booktest/login.html')
+
+def login_check(request):
+    '''登錄校驗試圖'''
+    # 1.獲取提交的用戶名和密碼
+
+    # 2.進行登錄的校驗
+
+    # 3.返回應答
